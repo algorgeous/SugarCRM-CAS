76f89a56beb9802ac0fcd684679d42577bf3c6c6
Alfred Bacon <bacon@gatech.edu>
2012-05-24 10:06:04 -0400

    Removed hard-coded CAS parameters. Using $sugar_config variable from config.php file.
    Some code cleanup.

--- CASAuthenticate.php
+++ CASAuthenticate.php
@@ -1,103 +1,103 @@
 <?php
+
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
-require_once('modules/Users/authentication/SugarAuthenticate/SugarAuthenticate.php');
-require_once('/usr/share/php/CAS.php');
+require_once 'modules/Users/authentication/SugarAuthenticate/SugarAuthenticate.php';
+
+global $sugar_config;
+require_once $sugar_config['cas']['library'];
 
 /********************************************************************
  * Module that allows Sugar to perform user authentication using
  *  CAS.
  *********************************************************************/
 
-class CASAuthenticate extends SugarAuthenticate {
+class CASAuthenticate extends SugarAuthenticate
+{
     var $userAuthenticateClass = 'CASAuthenticateUser';
     var $authenticationDir = 'CASAuthenticate';
-      
-      function CASAuthenticate(){
-      parent::SugarAuthenticate();
-                  require_once('modules/Users/authentication/'. $this->authenticationDir . '/'. $this->userAuthenticateClass . '.php');
-        $this->userAuthenticate = new $this->userAuthenticateClass();
-            $this->doCASAuth();           
-          } 
 
-      
-        function doCASAuth(){
-         @session_start();
-                                 
+    function CASAuthenticate()
+    {
+      parent::SugarAuthenticate();
+      require_once('modules/Users/authentication/'. $this->authenticationDir . '/'. $this->userAuthenticateClass . '.php');
+        $this->userAuthenticate = new $this->userAuthenticateClass();
+        $this->doCASAuth();
+    }
+
+    function doCASAuth()
+    {
+        @session_start();
+
         // Don't try to login if the user is logging out
         if (isset($_REQUEST['action']) && $_REQUEST['action'] == 'Logout') {
             $this->logout();
-        }
         // If the user is already authenticated, do this.
-        elseif (isset($_SESSION['authenticated_user_id']) ) {
-            $this->sessionAuthenticate();   
+        } elseif (isset($_SESSION['authenticated_user_id']) ) {
+            $this->sessionAuthenticate();
             return;
-        }
-                   // Try to log the user in via SSO
-        else {
+        } else {
+          // Try to log the user in via SSO
             if ($this->userAuthenticate->loadUserOnLogin() == true) {
               parent::postLoginAuthenticate();
-              header( 'Location: http://10.5.142.205' );
-                 }
-          else {
-           
+              header('Location: ' . $sugar_config['site_url']);
+            } else {
+
             die(); //I should redirect here.  I'm not sure on the syntax -- sorry.
-                       } //end nested else.
+            } //end nested else.
         } // end top else.
-        } //end doCASAuth()
- 
-     
-        function sessionAuthenticate(){
-        global $module, $action, $allowed_actions;
+    } //end doCASAuth()
+
+
+    function sessionAuthenticate()
+    {
+        global $module, $action, $allowedActions;
         $authenticated = false;
-        $allowed_actions = array ("Authenticate", "Login"); // these are actions where the user/server keys aren't compared
+        $allowedActions = array ("Authenticate", "Login"); // these are actions where the user/server keys aren't compared
         if (isset ($_SESSION['authenticated_user_id'])) {
             $GLOBALS['log']->debug("We have an authenticated user id: ".$_SESSION["authenticated_user_id"]);
             $authenticated = $this->postSessionAuthenticate();
-        } else
-            if (isset ($action) && isset ($module) && $action == "Authenticate" && $module == "Users") {
+        } else if (isset ($action) && isset ($module) && $action == "Authenticate" && $module == "Users") {
             $GLOBALS['log']->debug("We are NOT authenticating user now.  CAS will redirect.");
-        } 
+        }
         return $authenticated;
-        } //end sessionAuthenticate()
+    } //end sessionAuthenticate()
 
-       
-      function postSessionAuthenticate(){
-        global $action, $allowed_actions, $sugar_config;
+
+    function postSessionAuthenticate()
+    {
+        global $action, $allowedActions, $sugar_config;
         $_SESSION['userTime']['last'] = time();
         $user_unique_key = (isset ($_SESSION['unique_key'])) ? $_SESSION['unique_key'] : '';
         $server_unique_key = (isset ($sugar_config['unique_key'])) ? $sugar_config['unique_key'] : '';
-        
+
         //CHECK IF USER IS CROSSING SITES
-        if (($user_unique_key != $server_unique_key) && (!in_array($action, $allowed_actions)) && (!isset ($_SESSION['login_error']))) {
-            
+        if (($user_unique_key != $server_unique_key) && (!in_array($action, $allowedActions)) && (!isset ($_SESSION['login_error']))) {
+
             session_destroy();
-            $post_login_nav = '';
+            $postLoginNav = '';
             if (!empty ($record) && !empty ($action) && !empty ($module)) {
-                $post_login_nav = "&login_module=".$module."&login_action=".$action."&login_record=".$record;
+                $postLoginNav = "&login_module=".$module."&login_action=".$action."&login_record=".$record;
             }
             $GLOBALS['log']->debug('Destroying Session User has crossed Sites');
-            //header("Location: index.php?action=Login&module=Users".$post_login_nav);
             sugar_cleanup(true);
-                        die(); 
+            die();
         }
         if (!$this->userAuthenticate->loadUserOnSession($_SESSION['authenticated_user_id'])) {
             session_destroy();
-            //header("Location: index.php?action=Login&module=Users");
             $GLOBALS['log']->debug('Current user session does not exist redirecting to login');
             sugar_cleanup(true);
-                        die(); 
-               }
+            die();
+        }
         $GLOBALS['log']->debug('Current user is: '.$GLOBALS['current_user']->user_name);
         return true;
-          }//end postSessionAuthenticate()
-  
+    } //end postSessionAuthenticate()
 
-
-            function logout() {
-        phpCAS::setDebug();
-        phpCAS::client(CAS_VERSION_2_0,'cas.cristoreyny.org',443,'cas');
+    function logout()
+    {
+        global $sugar_config;
+        phpCAS::client(CAS_VERSION_2_0, $sugar_config['cas']['hostname'], $sugar_config['cas']['port'], $sugar_config['cas']['uri'], $sugar_config['cas']['changeSessionID']);
         phpCAS::setNoCasServerValidation();
-        phpCAS::logout(); 
-      }
+        phpCAS::logout();
+    }
 
-}//end CASAuthenticate class 
+} //end CASAuthenticate class
--- CASAuthenticateUser.php
+++ CASAuthenticateUser.php
@@ -1,44 +1,53 @@
 <?php
+
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
 require_once('modules/Users/authentication/LDAPAuthenticate/LDAPConfigs/default.php');
 require_once('modules/Users/authentication/SugarAuthenticate/SugarAuthenticateUser.php');
-include_once('/usr/share/php/CAS.php');
-define('DEFAULT_PORT', 389);
+global $sugar_config;
+require_once $sugar_config['cas']['library'];
+
+/**
+ * This is called when a user logs in
+ *
+ * @param STRING $name
+ * @param STRING $password
+ * @return boolean
+ */
 class CASAuthenticateUser extends SugarAuthenticateUser {
 
-    /********************************************************************************
-     * This is called when a user logs in 
+    /**
+     * This is called when a user logs in
      *
      * @param STRING $name
      * @param STRING $password
      * @return boolean
-     ********************************************************************************/
-     
+     */
      function loadUserOnLogin() {
         $name=$this->authUser();
         if(empty($name)){
-            return false;              
+            return false;
         }
         else{
-            return true; 
+            return true;
         }
-     }//end loadUserOnlogin()
-  
-   /**********************************************************************************************************************
+     } //end loadUserOnlogin()
+
+   /**
     * Attempt to authenticate the user via CAS SSO
-    ***********************************************************************************************************************/    
+    */
     function authUser() {
-        $cas_server = 'cas.cristoreyny.org';
-        phpCAS::client(CAS_VERSION_2_0,$cas_server,443,'cas');
+	global $sugar_config;
+        phpCAS::setDebug();
+        phpCAS::client(CAS_VERSION_2_0, $sugar_config['cas']['hostname'], $sugar_config['cas']['port'], $sugar_config['cas']['uri'], $sugar_config['cas']['changeSessionID']);
         phpCAS::setNoCasServerValidation();
         phpCAS::forceAuthentication();
         $authenticated = phpCAS::isAuthenticated();
         if ($authenticated)
-        {   
+        {
             $user_name = phpCAS::getUser();
 
-		    $dbresult = $GLOBALS['db']->query("SELECT id, status FROM users WHERE user_name='" . $user_name . "' AND deleted = 0");			
+	    $dbresult = $GLOBALS['db']->query("SELECT id, status FROM users WHERE user_name='" . $user_name . "' AND deleted = 0");			
             //user already exists use this oe
             if($row = $GLOBALS['db']->fetchByAssoc($dbresult)){
                 if($row['status'] != 'Inactive')
@@ -46,21 +55,16 @@ class CASAuthenticateUser extends SugarAuthenticateUser {
                 else
                     return '';
             }
-       	    echo 'Not authorized user. You may need to ask an administrator to give you access to SugarCRM. You may try logging in again <a href="https://'.$cas_server.':443/cas/logout?service=http://'.$_SERVER['SERVER_NAME'].'">here</a>';
-		    die();
+       	    echo 'Not authorized user. You may need to ask an administrator to give you access to SugarCRM. You may try logging in again <a href="https://' . $sugar_config['cas']['hostname'] . ':443/cas/logout?service=http://' . $_SERVER['SERVER_NAME'] . '">here</a>';
+	    die();
             return ""; // SSO authentication was successful
         }
         else // not authenticated in CAS.
         {
             return;
         }
-    }//end authenticateSSO();
+    } //end authenticateSSO();
 
-      
-/************************************************************************************************************/
-/************************************************************************************************************/
-
-}//End CASAuthenticateUSer class.
+} //End CASAuthenticateUser class.
 
 ?>
-
